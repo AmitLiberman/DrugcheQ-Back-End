37580330a3ac3d0adb8a7dc9ec5fcaf29d26ce71
Amit Liberman <amitliberman224@gmail.com>
2021-01-17 15:05:24 +0200

    no sevirty problem bug fixed

--- drug_api.py
+++ drug_api.py
@@ -15,11 +15,13 @@ def find_serials(drug_list):
         x = requests.get('https://rxnav.nlm.nih.gov/REST/rxcui.json?name=' + drug)
         response_as_dict = json.loads(x.text)
         drugs_serial_number.append(response_as_dict['idGroup']['rxnormId'][0])
+    print(drugs_serial_number)
     return drugs_serial_number
 
 
 # parse the response JSON file from the API and build dictionary with relevant data
 def build_interaction_dict(response_as_dict):
+    print(response_as_dict)
     full_interaction_type = response_as_dict['fullInteractionTypeGroup'][0]['fullInteractionType']
     interaction_dict = {}
     for i in range(len(full_interaction_type)):
@@ -27,12 +29,12 @@ def build_interaction_dict(response_as_dict):
         interaction_dict[i]['drug1'] = full_interaction_type[i]['minConcept'][0]['name']
         interaction_dict[i]['drug2'] = full_interaction_type[i]['minConcept'][1]['name']
         interaction_dict[i]['description'] = full_interaction_type[i]['interactionPair'][0]['description']
-        severity_interaction_type = response_as_dict['fullInteractionTypeGroup'][1]['fullInteractionType']
-        for j in range(len(severity_interaction_type)):
-
-            if interaction_dict[i]['drug1'] in severity_interaction_type[j]['comment'] and interaction_dict[i]['drug2'] \
-                    in severity_interaction_type[j]['comment']:
-                interaction_dict[i]['severity'] = severity_interaction_type[j]['interactionPair'][0]['severity']
+        if len(response_as_dict['fullInteractionTypeGroup']) > 1:
+            severity_interaction_type = response_as_dict['fullInteractionTypeGroup'][1]['fullInteractionType']
+            for j in range(len(severity_interaction_type)):
+                if interaction_dict[i]['drug1'] in severity_interaction_type[j]['comment'] and interaction_dict[i]['drug2'] \
+                        in severity_interaction_type[j]['comment']:
+                    interaction_dict[i]['severity'] = severity_interaction_type[j]['interactionPair'][0]['severity']
     return interaction_dict
 
 
