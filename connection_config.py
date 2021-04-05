7005505f673dda9cd5b3af3c3c7a271270bb5243
Amit Liberman <amitliberman224@gmail.com>
2021-04-05 12:28:50 +0300

    added credintails

---
+++ connection_config.py
@@ -0,0 +1,7 @@
+connection_config = """
+        dbname='d984k5vt1jr9rs'
+        user='vrlaozxymgbowo'
+        password='57941d7c9247c6cd0bc40e4467ed998eece037cc7c5dcafe6515820ec4095f0f'
+        host='ec2-54-76-215-139.eu-west-1.compute.amazonaws.com'
+        port='5432'
+        """
