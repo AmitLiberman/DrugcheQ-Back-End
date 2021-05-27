e41f79fccb4f66c23fb343b72b3db6221191758a
Amit Liberman <amitliberman224@gmail.com>
2021-05-27 15:45:21 +0300

    complete inserting to DB

--- validator/drug_validator.py
+++ validator/drug_validator.py
@@ -33,19 +33,46 @@ def send_req(drug_name):
 def db_check(english_name, hebrew_name):
     data_base = DB()
     temp_drug_list = data_base.fetch_row_data(
-        "SELECT * FROM drug_name WHERE hebrew_name=%s and english_name=%s", hebrew_name,english_name)
+        "SELECT * FROM drug_name WHERE hebrew_name=%s and english_name=%s", hebrew_name, english_name)
     data_base.close_connection()
     print(temp_drug_list)
+    if len(temp_drug_list) > 0:
+        return True
+    else:
+        return False
+
+
+def insert_data(new_drug_details):
+    data_base = DB()
+    new_drug_data = (
+        new_drug_details['remedy_number'],
+        new_drug_details['english_name'],
+        new_drug_details['hebrew_name'],
+        new_drug_details['ingredients'],
+        new_drug_details['details'],
+        new_drug_details['dosage_form'],
+        new_drug_details['how_taking'],
+        new_drug_details['prescription'],
+        new_drug_details['health_basket'],)
+    postgres_insert_query = """ INSERT INTO drug_name (remedy_number, english_name, hebrew_name,ingredients,details, dosage_form,\
+    how_taking,prescription,health_basket) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    data_base.insert_data_row(postgres_insert_query, new_drug_data)
+    data_base.close_connection()
 
 
 def validate(drug_name):
     response = send_req(drug_name)
     print(response)
-    remedy_number = "-".join(response['results'][0]['dragRegNum'])
+    if len(response['results']) == 0:
+        return
     english_name = response['results'][0]['dragEnName']
     hebrew_name = response['results'][0]['dragHebName']
-    db_check( english_name, hebrew_name)
+    drug_exist = db_check(english_name, hebrew_name)
+    if drug_exist:
+        print("Drug already exist in DB")
+        return
 
+    remedy_number = response['results'][0]['dragRegNum'].replace(" ", "-")
     ingredients = ''
     for key, value in response['results'][0]['activeComponents'][0].items():
         ingredients += value + ';'
@@ -61,7 +88,14 @@ def validate(drug_name):
         health_basket = "כן"
     else:
         health_basket = "לא"
+    new_drug_details = {'english_name': english_name, 'hebrew_name': hebrew_name, 'remedy_number': remedy_number,
+                        'ingredients': ingredients, 'details': details, 'dosage_form': dosage_form,
+                        'how_taking': how_taking,
+                        'prescription': prescription, 'health_basket': health_basket}
+    print("Inserting new drug row to DB")
+    print(new_drug_details)
+    insert_data(new_drug_details)
 
 
 if __name__ == '__main__':
-    validate("יומירה")
+    validate("אדמלוג")
